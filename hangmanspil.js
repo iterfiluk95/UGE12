f36a575c50fe95a735df5a737985c482baf4352f
Lili <92336105+iterfiluk95@users.noreply.github.com>
2022-03-24 22:29:09 +0100

    Add files via upload

---
+++ hangmanspil.js
@@ -0,0 +1,116 @@
+const wordE1 = document.getElementById('word');
+const wrongLettersE1 = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-button');
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
+const finalMessage = document.getElementById('final-message');
+
+const figureParts= document.querySelectorAll(".figure-part");
+
+const words = ["aback", "abaft", "abandoned", "absorbing", "abstracted", "absurd", "abundant", "abusive", "acceptable", "actually", "adamant", "adaptable", "addicted", "bloody", "bizarre", "delightful", "demonic", "excellent", "excited", "electric", "elegant", "free", "freezing", "frequent", "glorious", "lonely", "guiltless", "gullible", "hilarious", "hissing", "historical", "hollow", "innocent", "intelligent", "interesting", "internal", "laughable", "likeable", "majestic", "makeshift", "melted", "opposite", "optimal", "orange", "ordinary", "organic", "necessary", "needless", "neighbor", "pointless", "poised", "polite", "political", "poor", "possessive", "romantic", "roomy", "rotten", "rough", "round", "royal", "sparkling", "special", "spectacular", "thundering", "tidy", "tight", "unsuitable", "untidy", "unused", "unusual", "whimsical", "whispering", "white", "young", "youthful", "yummy"];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+const correctLetters = [];
+const wrongLetters = [];
+
+//Show hidden word
+function displayWord(){
+    wordE1.innerHTML = `
+    ${selectedWord
+    .split('')
+    .map(
+        letter =>`
+        <span class="letter">
+        ${correctLetters.includes(letter) ? letter : ''}
+        </span>
+        `
+    )
+    .join('')}
+    `;
+
+    const innerWord = wordE1.innerText.replace(/\n/g, '');
+
+    if(innerWord === selectedWord){
+        finalMessage.innerText = 'TILLYKKE DU VANDT!';
+        popup.style.display= 'flex';
+    }
+}
+
+// Update the wrong letters
+function updateWrongLetterE1(){
+    //Display wrong letters
+    wrongLettersE1.innerHTML = `
+    ${wrongLetters.length > 0 ? '<p>Forkerte bogstaver:</p>' : ''}
+    ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+    `;
+
+    //Display parts
+    figureParts.forEach((part,index) => {
+        const errors = wrongLetters.length;
+
+        if(index < errors) {
+            part.style.display = 'block'
+        }
+        else{
+            part.style.display = 'none';
+        }
+    });
+
+    //Check if lost
+    if(wrongLetters.length === figureParts.length){
+        finalMessage.innerText = 'Du tabte spillet :(';
+        popup.style.display = 'flex';
+    }
+}
+
+//Show notification
+function showNotification(){
+    notification.classList.add('show');
+
+    setTimeout(() => {
+        notification.classList.remove('show');
+    }, 2000);
+}
+
+//Keydown letter press
+window.addEventListener('keydown', e =>{
+    if(e.keyCode >= 65 && e.keyCode <=90){
+        const letter = e.key;
+
+        if(selectedWord.includes(letter)){
+            if(!correctLetters.includes(letter)){
+                correctLetters.push(letter);
+
+                displayWord();
+            } else{
+                showNotification();
+            }
+        } else{
+            if(!wrongLetters.includes(letter)){
+                wrongLetters.push(letter);
+
+                updateWrongLetterE1();
+            } else{
+                showNotification();
+            }
+        }
+    }
+});
+
+//Restarts the game and play again
+playAgainBtn.addEventListener('click', () => {
+    //Empty arrays
+    correctLetters.splice(0);
+    wrongLetters.splice(0);
+
+    selectedWord = words[Math.floor(Math.random() * words.length)];
+
+    displayWord();
+
+    updateWrongLetterE1();
+
+    popup.style.display = 'none';
+});
+
+displayWord();
